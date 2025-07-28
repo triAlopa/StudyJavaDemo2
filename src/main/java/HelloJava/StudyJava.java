c8898ef825099cac80631ecbfcb6bc32332e8697
triAlopa <buzhey6@gmail.com>
2025-07-28 17:30:18 +0800

    commit

---
+++ src/main/java/HelloJava/StudyJava.java
@@ -0,0 +1,8 @@
+package HelloJava;
+
+public class StudyJava {
+    //今天 25.7.28 真得把 JavaSe 学完了起码 😥
+    public static void main(String[] args) {
+        System.out.println("Start study JavaSe!!!");
+    }
+}
