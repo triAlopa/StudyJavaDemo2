6133deff707ad519768c29384edc56074208fbec
triAlopa <buzhey6@gmail.com>
2025-07-29 16:18:51 +0800

    commit

--- src/main/java/StuDemo7_29/SearchDemo/BasicSearchDemo1.java
+++ src/main/java/StuDemo7_29/SearchDemo/BasicSearchDemo1.java
@@ -5,10 +5,15 @@ package StuDemo7_29.SearchDemo;
 //就是在一堆数据里面是否存在 某个数据
 public class BasicSearchDemo1 {
     public static void main(String[] args) {
+        // 定义一个整型数组
         int [] arr=new int[]{1,12,3,4,5,20,50,9,0};
+        // 定义一个整型变量
         int num=6;
+        // 调用searchFun方法，传入数组arr和变量num，将返回值赋给result
         boolean result = searchFun(arr, num);
+        // 如果result为true，则输出"执行顺序算法，存在"
         if(result) System.out.println("执行顺序算法，存在");
+        // 否则输出"不存在"
         else System.err.println("不存在");
     }
     private static boolean searchFun(int[] arr,int num){
