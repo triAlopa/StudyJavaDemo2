2e072e858b715a80675dc619576421a274d50651
triAlopa <buzhey6@gmail.com>
2025-09-01 17:32:14 +0800

    final commit

--- src/main/java/stuDemo9_01/myIOe/E1.java
+++ src/main/java/stuDemo9_01/myIOe/E1.java
@@ -1,13 +1,13 @@
 package stuDemo9_01.myIOe;
 
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
+import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,33 +18,34 @@ import java.util.regex.Pattern;
              获取女生名字：http://www.haoming8.cn/baobao/7641.html
         */
 public class E1 {
-/**
- * 主方法：用于从网络爬取百家姓和男孩女孩名字数据，并进行处理
- * @param args 命令行参数
- * @throws IOException 可能抛出的IO异常
- */
+    /**
+     * 主方法：用于从网络爬取百家姓和男孩女孩名字数据，并进行处理
+     *
+     * @param args 命令行参数
+     * @throws IOException 可能抛出的IO异常
+     */
     public static void main(String[] args) throws IOException {
-    // 定义百家姓网页URL
+        // 定义百家姓网页URL
         String familyNameNet = "https://hanyu.baidu.com/shici/detail?pid=0b2f26d4c0ddb3ee693fdb1137ee1b0d&from=kg0";
-    // 定义男孩名字网页URL
+        // 定义男孩名字网页URL
         String boyNameNet = "http://www.haoming8.cn/baobao/10881.html";
-    // 定义女孩名字网页URL
+        // 定义女孩名字网页URL
         String girlNameNet = "http://www.haoming8.cn/baobao/7641.html";
 
         //临时处理数据：爬取百家姓网页内容并提取百家姓
         String familyNameStr = webCrawler(familyNameNet);
-        ArrayList<String> familyTempList = getData(familyNameStr, "([\\u4e00-\\u9fa5]{4})(，|。)",1);
+        ArrayList<String> familyTempList = getData(familyNameStr, "([\\u4e00-\\u9fa5]{4})(，|。)", 1);
 
         //临时处理数据：爬取男孩名字网页内容并提取男孩名字
         String boyNameStr = webCrawler(boyNameNet);
-        ArrayList<String> boyTempList = getData(boyNameStr, "([\\u4e00-\\u9fa5]{2})(、|。)",1);
+        ArrayList<String> boyTempList = getData(boyNameStr, "([\\u4e00-\\u9fa5]{2})(、|。)", 1);
 
         //临时处理数据：爬取女孩名字网页内容并提取女孩名字
         String girlNameStr = webCrawler(girlNameNet);
-        ArrayList<String> girlTempList = getData(girlNameStr, "(.. ){4}(..)",0);
+        ArrayList<String> girlTempList = getData(girlNameStr, "(.. ){4}(..)", 0);
 
         //处理百家姓数据：将每个姓氏拆分为单个字符并添加到列表中
-        ArrayList<String> familyList=new ArrayList<>();
+        ArrayList<String> familyList = new ArrayList<>();
         for (String str : familyTempList) {
             for (int i = 0; i < str.length(); i++) {
                 familyList.add(str.charAt(i) + "");
@@ -52,15 +53,15 @@ public class E1 {
         }
 
         //处理男孩名字数据：去重处理
-        ArrayList<String> boyList=new ArrayList<>();
+        ArrayList<String> boyList = new ArrayList<>();
         for (String str : boyTempList) {
-            if(!boyList.contains(str)){
+            if (!boyList.contains(str)) {
                 boyList.add(str);
             }
         }
 
         //处理女孩名字数据：分割字符串并添加到列表中
-        ArrayList<String> girlList=new ArrayList<>();
+        ArrayList<String> girlList = new ArrayList<>();
         for (String str : girlTempList) {
             String[] arr = str.split(" ");
             for (int i = 0; i < arr.length; i++) {
@@ -68,17 +69,72 @@ public class E1 {
             }
         }
 
+        ArrayList<String> list = getInfos(familyList, boyList, girlList, 50, 60);
+        Collections.shuffle(list);
+        //System.out.println(list.toString());
+        //写入本地文件
+        BufferedWriter bw=new BufferedWriter(new FileWriter("names.txt"));
+        for (String str : list) {
+            bw.write(str);
+            bw.newLine();
+        }
+        bw.close();
+    }
+
+/**
+ * 生成指定数量的男生和女生姓名信息，并添加年龄和性别标识
+ * @param familyList 姓氏列表
+ * @param boyList 男生名字列表
+ * @param girlList 女生名字列表
+ * @param boyCount 需要生成的男生数量
+ * @param girlCount 需要生成的女生数量
+ * @return 返回包含姓名、性别和年龄信息的ArrayList
+ */
+    private static ArrayList<String> getInfos(ArrayList<String> familyList, ArrayList<String> boyList, ArrayList<String> girlList, int boyCount, int girlCount) {
+
+        //添加男生姓名
+        HashSet<String> boyHs = new HashSet<>();
+        while (true) {
+            if (boyHs.size() == boyCount) break;
+            Collections.shuffle(familyList);
+            Collections.shuffle(boyList);
+            boyHs.add(familyList.get(0) + boyList.get(0));
+        }
+
+        //添加女生姓名
+        HashSet<String> girlHs = new HashSet<>();
+        while (true) {
+            if (girlHs.size() == girlCount) break;
+            Collections.shuffle(familyList);
+            Collections.shuffle(girlList);
+            girlHs.add(familyList.get(0) + girlList.get(0));
+        }
+        //添加到最终列表，并进行加入年龄、性别
+        Random r = new Random();
+        ArrayList<String> list = new ArrayList<>();
+        //18~25 男
+        for (String boyName : boyHs) {
+            int age = r.nextInt(8) + 18;
+            list.add(boyName + "-男-" + age);
+        }
+        //18~27 女
+        for (String girlName : girlHs) {
+            int age = r.nextInt(10) + 18;
+            list.add(girlName + "-女-" + age);
+        }
+
+        return list;
     }
 
     private static ArrayList<String> getData(String nameStr, String regex, int index) {
         //1.创建 数组列表保存数据返回
-        ArrayList<String> list=new ArrayList<>();
+        ArrayList<String> list = new ArrayList<>();
         //2.创建 表达式规则的容器，并处理数据；
         Pattern pattern = Pattern.compile(regex);    // 编译正则表达式，创建模式对象
         Matcher matcher = pattern.matcher(nameStr);   // 创建匹配器，用于在输入字符串中查找匹配项
 
         //3.爬取添加
-        while (matcher.find()){                     // 循环查找所有匹配项
+        while (matcher.find()) {                     // 循环查找所有匹配项
             list.add(matcher.group(index));          // 将匹配到的指定组索引的内容添加到列表中
         }
         return list;                                // 返回包含所有匹配结果的列表
